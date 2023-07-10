9b2f894d3ea2d4bfd97dabd5a6cdb3babd763929
M E I Y <139093934+Ta1thut@users.noreply.github.com>
2023-07-10 10:40:13 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() //var return
+{
+	/*std::*/cout  << "Student Code : 66-060216-2119-4 \n";
+			cout  << "Student Name : Tai-thut Phonsakul \n";
+			cout  << "    Division : Information Technology \n";
+			cout  << "        Year : 1 \n" << "        Room : RB \n" << "         Sex : Male \n" << "         Age : 18 \n";
+	// \n = endl
+	system("pause"); //wfi
+	return 0;
+}
