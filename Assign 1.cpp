dcc26a7aac2de8a6ec8cb288d89080cf54968a6a
M E I Y <139093934+Ta1thut@users.noreply.github.com>
2023-07-16 16:44:08 +0700

    Add files via upload

---
+++ Assign 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	cout << "     *******\n";
+	cout << "    ***\n";
+	cout << "   **\n";
+	cout << "  **\n";
+	cout << "   ***\n";
+	cout << "     *******\n";
+
+}
