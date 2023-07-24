d5702f616a679f545885f93479e38ce3b76605db
M E I Y <139093934+Ta1thut@users.noreply.github.com>
2023-07-24 09:49:26 +0700

    Add files via upload

--- True.cpp
+++ True.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 int main(){
 	int score;
-	cout << "Enter Your Score : ";
+	cout << "Input C++ Score : ";
 	cin >> score;
-	cout << "SCORE = " << score << endl ;
-	cout <<"You " << ((score >= 50 )?"pass! ":"fail! ") << endl ;
+	cout <<"You " << ((score >= 50 )?"pass":"fail") << " with score " << score << endl ;
 	system("pause");
 	return(0);		
 }
